4e943e7d196e4d1dbf9f7fb60e370579f3236799
Naila <62919728+NailaAzmie@users.noreply.github.com>
2020-04-14 15:18:32 +0800

    Create variable_lokal.cpp

---
+++ variable_lokal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std ;
+
+int contoh_function()
+{
+	int a = 9 ;
+	return a ;
+}
+
+int contoh_function2()
+{
+	int n = 8 ;
+	return n ;
+}
+
+int main()
+ {
+ 	int a = 1 ;
+ 	cout <<"Ini adalah fungsi utama " <<  a << endl ;
+ 	cout << "Ini adalah contoh function 1 : " <<  contoh_function() << endl ;
+ 	int n = 2 ;
+ 	cout << "Ini adalah fungsi utama " << n << endl ;
+ 	cout <<  "Ini adalah contoh function 2 : " << contoh_function2() <<endl;
+ }
